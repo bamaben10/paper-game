32918ec1e761b53e4c9cc2f5ea55a3e8a3119c75
Benjamin Salmon <bensalmon8@gmail.com>
2022-12-10 05:53:06 -0600

    change

--- script.js
+++ script.js
@@ -1,55 +1,3 @@
-// const selectionButtons = document.querySelectorAll("[data-selection]");
-// const finalColumn = document.querySelector("[data-final-column]");
-// const SELECTIONS = [
-//   {
-//     name: "rock",
-//     beats: "scissors",
-//   },
-//   {
-//     name: "paper",
-//     beats: "rock",
-//   },
-//   {
-//     name: "scissors",
-//     beats: "paper",
-//   },
-// ];
-
-// selectionButtons.forEach((selectionButton) => {
-//   selectionButton.addEventListener("click", (e) => {
-//     const selectionName = selectionButton.dataset.selection;
-//     const selection = SELECTIONS.find(
-//       (selection) => selection.name === selectionName
-//     );
-//     makeSelection(selection);
-//   });
-// });
-
-// function makeSelection(selection) {
-//   const computerSelection = randomSelection();
-//   const yourWinner = isWinner(selection, computerSelection);
-//   const computerWinner = isWinner(computerSelection, selection);
-//   console.log(computerSelection);
-//   addSelectionResult(computerSelection, computerWinner);
-//   addSelectionResult(selection, yourWinner);
-// }
-
-// function addSelectionResult(selection, winner) {
-//   const div = document.createElement("div");
-//   div.classList.add("result-selection");
-//   if (winner) div.classList.add("winner");
-//   finalColumn.after(div);
-// }
-
-// function isWinner(selection, opponenetSelection) {
-//   return selection.beats === opponenetSelection.name;
-// }
-
-// function randomSelection() {
-//   const randomIndex = Math.floor(Math.random() * SELECTIONS.length);
-//   return SELECTIONS[randomIndex];
-// }
-
 const selectionButtons = document.querySelectorAll("[data-selection]");
 const finalColumn = document.querySelector("[data-final-column]");
 const computerScoreSpan = document.querySelector("[data-computer-score]");
@@ -57,17 +5,14 @@ const yourScoreSpan = document.querySelector("[data-your-score]");
 const SELECTIONS = [
   {
     name: "rock",
-    emoji: "✊",
     beats: "scissors",
   },
   {
     name: "paper",
-    emoji: "✋",
     beats: "rock",
   },
   {
     name: "scissors",
-    emoji: "✌",
     beats: "paper",
   },
 ];
